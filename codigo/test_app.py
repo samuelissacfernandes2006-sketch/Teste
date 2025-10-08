6acd3bd1f08ba0542db26c63d99974adf6fa7baa
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-08 14:50:23 -0300

    Update test_app.py

--- codigo/test_app.py
+++ codigo/test_app.py
@@ -1,5 +1,11 @@
 import APP
 
-def test_login():
-  resultado = APP.login(user:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db))
-  assert resultado == {"mensagem":f"login {login_atual}"}
+def test_de_secretkey:
+  valordachave = APP.SECRET_KEY
+  assert valordachave == "teste123"
+def test_de_algorithm:
+  valordachave = APP.ALGORITHM
+  assert valordachave == "HS256"
+def test_de_expire:
+  valordachave = APP.ACCESS_TOKEN_EXPIRE_MINUTES
+  assert valordachave == 2
