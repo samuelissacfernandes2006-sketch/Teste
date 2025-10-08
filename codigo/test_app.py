b7ec01eeb678b610b18cbdb77c1b2dc1e97c6803
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-08 14:33:13 -0300

    Update test_app.py

--- codigo/test_app.py
+++ codigo/test_app.py
@@ -1,1 +1,5 @@
+import APP
 
+def test_login():
+  resultado = APP.login(user:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db))
+  assert resultado == {"mensagem":f"login {login_atual}"}
