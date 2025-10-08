5d21e662061cb9ba8183e1278359ee24a3ca73f5
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-08 14:51:27 -0300

    Update test_app.py

--- codigo/test_app.py
+++ codigo/test_app.py
@@ -1,11 +1,11 @@
 import APP
 
-def test_de_secretkey:
+def test_de_secretkey():
   valordachave = APP.SECRET_KEY
   assert valordachave == "teste123"
-def test_de_algorithm:
+def test_de_algorithm():
   valordachave = APP.ALGORITHM
   assert valordachave == "HS256"
-def test_de_expire:
+def test_de_expire():
   valordachave = APP.ACCESS_TOKEN_EXPIRE_MINUTES
   assert valordachave == 2
