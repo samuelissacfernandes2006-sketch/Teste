27d651efa557aaeebdd38b6fe1b914311d300e65
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-10 10:50:31 -0300

    Update test_requests.py

--- test_requests.py
+++ test_requests.py
@@ -7,5 +7,5 @@ def test_GET():
   toencode = {"sub":"teste"}
   toencode.update({"exp": datetime.now(timezone.utc) + timedelta(minutes = 2)})
   tokencodificado = jwt.encode(toencode,"teste123",algorithm= "HS256")
-  resultado = request("get","http://localhost:3019/items/-1", headers={"api-head":tokencodificado},verify= False)
+  resultado = request("get","http://[::1]:3019/items/-1", headers={"api-head":tokencodificado},verify= False)
   assert resultado.json() == {'mensagem': 'ITEM NÃO ENCONTRADO HTTP_404'}
