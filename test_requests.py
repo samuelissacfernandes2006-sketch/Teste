8eea54305ce8fef3e4cd7f5d731366be267e407f
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-10 11:00:19 -0300

    Update test_requests.py

--- test_requests.py
+++ test_requests.py
@@ -7,5 +7,5 @@ def test_GET():
   toencode = {"sub":"teste"}
   toencode.update({"exp": datetime.now(timezone.utc) + timedelta(minutes = 2)})
   tokencodificado = jwt.encode(toencode,"teste123",algorithm= "HS256")
-  resultado = request("get","http://[::1]:3019/items/-1", headers={"api-head":tokencodificado},verify= False)
-  assert resultado.json() == {'mensagem': 'ITEM NÃO ENCONTRADO HTTP_404'}
+  resultado = request("get","https://myfin-financial-management.bubbleapps.io/api/1.1/obj/category/", headers={"Authotization":"Bearer " +tokencodificado})
+  assert resultado.json() is not None
