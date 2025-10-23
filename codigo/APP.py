3206272fc80fee6b677dcfec3d3625ef2bc0adef
samuelissacfernandes2006-sketch <samuelissacfernandes2006@gmail.com>
2025-10-23 09:52:21 -0300

    Update APP.py

--- codigo/APP.py
+++ codigo/APP.py
@@ -1,14 +1,13 @@
-from typing import Annotated, Union
 from pydantic import BaseModel
-from fastapi import FastAPI, Depends, HTTPException, status, Header
+from fastapi import FastAPI, Depends,Header
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.orm import sessionmaker, declarative_base, Session 
-from jose import jwt, JWTError
+from jose import jwt
 from jwt import InvalidSignatureError, ExpiredSignatureError
 from datetime import datetime, timedelta, timezone
+from zoneinfo import ZoneInfo
 from passlib.context import CryptContext
-
+import psycopg2
+#secrets
 SECRET_KEY = "teste123"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 2
@@ -16,44 +15,45 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 2
 pwd_context = CryptContext(schemes=["des_crypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") #rota "token" para obter token
 
-#teste de login por usuario e senha
-login_atual= {"usuario":None,"senha": None, "token": None}
-
-
+def criar_conexão():
+    conexão = None
+    try:
+        conexão  = psycopg2.connect(host="some-postgres",port="5432",user="postgres",password="Jen28082006@*.")
+        conexão.set_session(autocommit=True)
+        cursorDB = conexão.cursor()
+        yield cursorDB
+    finally:
+        if conexão:
+            conexão.close()
+def criar_tabelas():
+    conexao_gerador = criar_conexão()
+    cursorDB = next(conexao_gerador)
+    
+    cursorDB.execute('''CREATE TABLE IF NOT EXISTS cliente (
+                    id_cliente SERIAL PRIMARY KEY,
+                    nome VARCHAR(255) NOT NULL,
+                    cpf VARCHAR(12) NOT NULL UNIQUE,
+                    data_de_nascimento DATE NOT NULL,
+                    esta_ativo BOOLEAN NOT NULL DEFAULT TRUE,
+                    CONSTRAINT chk_cpf_formato CHECK ( cpf ~ '^\d{11}$' )); ''')
+    cursorDB.execute('''CREATE TABLE IF NOT EXISTS produto (
+                    id_produto SERIAL PRIMARY KEY,
+                    nome_produto VARCHAR(255) NOT NULL UNIQUE,
+                    categoria VARCHAR(255) NOT NULL,
+                    valor float NOT NULL,
+                    esta_ativo BOOLEAN NOT NULL DEFAULT TRUE ); ''')
+    cursorDB.execute('''CREATE TABLE IF NOT EXISTS vendas (
+                    id_venda SERIAL PRIMARY KEY,
+                    id_cliente int REFERENCES cliente(id_cliente),
+                    id_produto int REFERENCES produto(id_produto),
+                    nome_produto VARCHAR(255) REFERENCES produto(nome_produto),
+                    quantidade int NOT NULL,
+                    valor float NOT NULL,
+                    data_compra TIMESTAMPTZ NOT NULL,
+                    data_confirmacao_compra TIMESTAMPTZ);''')
+criar_tabelas()
 
 app = FastAPI()
-
-Base = declarative_base()
-
-class UserDB(Base):
-    __tablename__ = "usuarios"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-
-class ItemDB(Base): 
-    __tablename__ = "itens"
-    id = Column("item_id", Integer, primary_key=True, index=True) 
-    name = Column(String, index=True)
-    price = Column(Integer)
-    
-SQLALCHEMY_DATABASE_URL = "sqlite:///Minhabasededados.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-Base.metadata.create_all(engine)  
-Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db():
-    db = Sessionlocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
 
 def validacao_de_token(token: str):
     try:
@@ -63,164 +63,235 @@ def validacao_de_token(token: str):
     except InvalidSignatureError:
         return {"mensagem":"ERRO DE ASSINATURA"}
 
-
-
-
-
-#verificar usuario na Userdb
-def get_user_by_username(db: Session, username: str):
-    return db.query(UserDB).filter(UserDB.username == username).first()
- 
-# Dependência para obter o usuário logado !!teste!////
-async def get_current_user(db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Não foi possível validar as credenciais",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        # Decodifica o token
-        payload = jwt.decode(login_atual["token"], SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = login_atual["usuario"]
-        if username is None:
-            raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Faça o login",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    except JWTError:
-        raise credentials_exception 
-    #procura o usuario na db e verifica a existencia
-    user = get_user_by_username(db, username=username)
-    if user is None:
-        raise credentials_exception
-    return user
-    #\teste!!!
-
-class Item(BaseModel):
-    item_name:str
-    item_price:float
+class Prod(BaseModel):
+    nome_produto: str
+    categoria: str
+    valor: float
+    
     class Config:
          from_attributes = True
+
+class ProdUPDATE(BaseModel):
+    nome_produto: str
+    categoria: str
+    valor: float
+    esta_ativo: bool
 
 class Token(BaseModel):
     access_token: str
     token_type: str
 
-class UserCreate(BaseModel):
-    usuario: str
-    senha: str
-@app.post("/")
-def login(user:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    db_user = get_user_by_username(db,username = user.username)
-    if not db_user or not verify_password(user.password,db_user.hashed_password): 
-        return {"mensagem":"usuario ou senha errados"}
-    login_atual["usuario"]=user.username
-    senha_codificada = get_password_hash(password=user.password)
-    login_atual["senha"]=senha_codificada
-    return {"mensagem":f"login {login_atual}"} 
+class ClienteCREATE(BaseModel):
+    nome: str
+    cpf: str
+    nascimento_yyyy_mm_dd: str 
 
-@app.post("/users/", response_model=UserCreate)
-def criar_usuario(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = get_user_by_username(db, username=user.usuario)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Usuário já registrado")
-    
-    hashed_password = get_password_hash(user.senha)
-    db_user = UserDB(username=user.usuario, hashed_password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return user
-'''@app.post("/users/", response_model=UserCreate)
-def criar_usuario_aberto(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = get_user_by_username(db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Usuário já registrado")
-    
-    hashed_password = get_password_hash(user.password)
-    db_user = UserDB(username=user.username, hashed_password=hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return user '''
-#criação de token :
-@app.post("/token")
-async def login_e_criar_token(data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = get_user_by_username(db, username=data.username)
-    
-    if not user or not verify_password(data.password, user.hashed_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="NOME DE USUARIO OU SENHA INCORRETOS",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    if login_atual["token"]:
-        login_atual["token"]= None
-    
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode = {"sub": login_atual["usuario"]}
-    if access_token_expires:
-        expire = datetime.now(timezone.utc) + access_token_expires
+class ClienteUPDATE(BaseModel):
+    nome: str
+    cpf: str
+    nascimento_yyyy_mm_dd: str 
+    esta_ativo: bool
+
+@app.get("/")
+def ler_raiz():
+    return{"TESTE":"API(localhost:3019/docs)"}
+@app.post("/cliente")
+def criar_cliente(cliente:ClienteCREATE, cursorDB: object = Depends(criar_conexão)):
+    try:
+        cursorDB.execute('''SELECT * FROM cliente WHERE cpf = %s''',(cliente.cpf,))
+
+    except psycopg2.errors.UndefinedFunction:
+        return {"mensagem":"CPF OU DATA INVALIDOS"}
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+        try:
+            cursorDB.execute('''INSERT INTO cliente (nome, cpf, data_de_nascimento) VALUES (%s, %s, %s)''',(cliente.nome, cliente.cpf, cliente.nascimento_yyyy_mm_dd,))
+        except psycopg2.errors.CheckViolation:
+            return {"mensagem": "CPF INVALIDO"}
+        except psycopg2.errors.UniqueViolation:
+            return {"mensagem":"CLIENTE JA EXISTENTES"}
+        except psycopg2.errors.StringDataRightTruncation:
+            return {"mensagem":"DADOS SOBRECARREGADOS"}
+        
+
+        return {"mensagem":f"cliente criado{cliente}"}
+
+@app.put("/cliente/{cpf_cliente}")
+def atualizar_cliente(cpf_cliente: int, cliente:ClienteUPDATE ,cursorDB: object = Depends(criar_conexão)):
+    cursorDB.execute('''SELECT * FROM cliente WHERE cpf = %s''',(cliente.cpf,))
+    resultado = []
+    linhas = cursorDB.fetchall()
+    for linha in linhas:
+        resultado+=linha    
+    if resultado == []:
+        return {"mensagem":"cliente não encontrado"}
+    
+    cursorDB.execute('''UPDATE cliente SET nome= %s WHERE cpf = %s''',(cliente.nome,cliente.cpf,))
+
+    cursorDB.execute('''UPDATE cliente SET data_de_nascimento = %s WHERE cpf = %s''',(cliente.nascimento_yyyy_mm_dd, cliente.cpf,))
+
+    cursorDB.execute('''UPDATE cliente SET esta_ativo = %s WHERE cpf = %s''',(cliente.cpf, cliente.cpf,))
+
+    cursorDB.execute('''UPDATE cliente SET cpf = %s WHERE cpf = %s''',(cliente.cpf, cliente.cpf,))
+
+
+    return {"mensagem":f"cliente atualizado com sucesso: {cliente}"}
+
+@app.delete("/cliente/{cpf_cliente}")
+def deletar_cliente(cpf_cliente: int, cursorDB: object = Depends(criar_conexão)):
+    cursorDB.execute('''SELECT * FROM cliente where cpf = %s''',(cpf_cliente,))
+    linhas = cursorDB.fetchall()
+
+    resultado = []
+    for linha in linhas:
+        resultado+=linha
+    if resultado== []:
+        return{"mensagem":"CLIENTE NÃO ENCONTRADO"}
+    
+    cursorDB.execute('''DELETE FROM cliente WHERE cpf = %s''',(cpf_cliente,))
+
+    return {"mensagem":"cliente deletado"}
+
+#criação de token :
+
+@app.post("/token")
+async def login_e_criar_token(data: OAuth2PasswordRequestForm = Depends()):
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = {"sub": data.username}
+    if access_token_expires:
+        expire = datetime.utcnow() + access_token_expires
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp":expire})
     encoded_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    login_atual["token"] = encoded_token
     return {"access_token": encoded_token, "token_type": "bearer"}
 
-
-@app.get("/items/{item_id}")
-def ler_item(item_id: int, db: Session = Depends(get_db),api_head: str = Header(default=None)):
-            #if login_atual["token"] is None:
-            #    return {"message":"CRIE UM TOKEN"}
+@app.get("/produto/{nome_do_produto}")
+def ler_item(nome_do_produto = str, api_head: str = Header(default=None),cursorDB: object = Depends(criar_conexão)):
             VT_resultado = validacao_de_token(token = api_head)
             if VT_resultado:
                 return {"mensagem":VT_resultado}
-
-            item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
-            if item is None:
+            cursorDB.execute('''SELECT * FROM produto WHERE nome_produto = %s''',(nome_do_produto,))
+            linhas = cursorDB.fetchall() 
+            produto = []
+            for linha in linhas:
+                produto+= linha
+            if produto == []:
                 return {"mensagem":"ITEM NÃO ENCONTRADO HTTP_404"}
-            return item
-mensagemdelogin ="Faça Login"
-@app.put("/items/{item_id}")
-def atualizar_item(item_id: int, item: Item, db:Session = Depends(get_db)):
-    verificação_de_usuario = db.query(UserDB).filter(UserDB.username == login_atual["usuario"])
-    if verificação_de_usuario is None:
-        return{"HTTP401":mensagemdelogin}
-    else:
-        testedeexistencia = db.query(ItemDB).filter(ItemDB.id == item_id).first()
-        if testedeexistencia is None:
-            return {"mensagem":"ITEM NÃO ENCONTRADO HTTP_404"}
-        else:
-            testedeexistencia.name = item.item_name
-            testedeexistencia.price = item.item_price
-            db.commit()
-            db.close()
-            return {"mensagem": "ITEM ATUALIZADO"}
-@app.post("/items/")
-async def criar_item(item: Item, db: Session = Depends(get_db)):
-    verificação_de_usuario = db.query(UserDB).filter(UserDB.username == login_atual["usuario"])
-    if verificação_de_usuario is None:
-        return{"HTTP401":mensagemdelogin}
-    else:
-        db_item = ItemDB(name=item.item_name, price=item.item_price)    
-        db.add(db_item)
-        db.commit()
-        db.refresh(db_item)
-        return db_item
+            return {"resultado":produto}
 
-@app.delete("/items/{item_id}")
-def deletar_item(item_id: int, db: Session = Depends(get_db)):
-    verificação_de_usuario = db.query(UserDB).filter(UserDB.username == login_atual["usuario"])
-    if verificação_de_usuario is None:
-        return{"HTTP401":mensagemdelogin}
+@app.put("/produto/{nome_do_produto}")
+def atualizar_item(nome_do_produto: str, prod:ProdUPDATE,cursorDB: object = Depends(criar_conexão)):
+    cursorDB.execute('''SELECT * FROM produto WHERE nome_produto = %s''',(nome_do_produto,))
+    resultado = []
+    linhas = cursorDB.fetchall()
+    for linha in linhas:
+        resultado+=linha    
+    if resultado == []:
+        return {"mensagem":"item não encontrado"}
+    
+    cursorDB.execute('''UPDATE produto SET categoria = %s WHERE nome_produto = %s''',(prod.categoria,prod.nome_produto,))
+
+    cursorDB.execute('''UPDATE produto SET valor = %s WHERE nome_produto = %s''',(prod.valor,prod.nome_produto,))
+
+    cursorDB.execute('''UPDATE produto SET esta_ativo = %s WHERE nome_produto = %s''',(prod.esta_ativo,nome_do_produto,))
+
+    cursorDB.execute('''UPDATE produto SET nome_produto = %s WHERE nome_produto = %s''',(prod.nome_produto, prod.nome_produto,))
+
+
+    return {"mensagem":f"produto criado com sucesso: {prod}"}
+@app.post("/produto")
+async def criar_item(produto: Prod,cursorDB: object = Depends(criar_conexão)):
+    nomedoproduto = produto.nome_produto
+    cursorDB.execute('''SELECT * FROM produto where nome_produto = %s''',(nomedoproduto,))
+    resultado = cursorDB.fetchall()
+
+    resul = []
+    for i in resultado:
+        resul+=i
+    if resul !=[]:
+        return{"mensagem":"item já existente"}
+    cursorDB.execute('''INSERT INTO produto (nome_produto, categoria, valor) VALUES (%s,%s,%s)''',(produto.nome_produto, produto.categoria,produto.valor,))
+
+    return{"mensagem":f"produto inserido na db {produto}"}
+    
+@app.delete("/produto/{nome_do_produto}")
+def desativar_item(nome_do_produto: str,cursorDB: object = Depends(criar_conexão)):
+    cursorDB.execute('''SELECT * FROM produto where nome_produto = %s''',(nome_do_produto,))
+    linhas = cursorDB.fetchall()
+
+    resultado = []
+    for linha in linhas:
+        resultado+=linha
+    if resultado== []:
+        return{"mensagem":"PRODUTO NÃO ENCONTRADO"}
+    
+    cursorDB.execute('''UPDATE produto SET esta_ativo = FALSE WHERE nome_produto = %s''',(nome_do_produto,))
+
+    return {"mensagem":f"produto {nome_do_produto} desativado"}
+
+@app.post("/vendas")
+def comprar_prod(CPF_do_cliente:str, nome_do_produto: str, quantidade: int , cursorDB: object = Depends(criar_conexão)):
+    try:
+        cursorDB.execute('''SELECT * FROM cliente where cpf = %s''',(CPF_do_cliente,))
+    except psycopg2.errors.UndefinedFunction:
+        return{"mensagem":"CPF INVALIDO"}
     else:
-        item = db.query(ItemDB).filter(ItemDB.id == item_id).first()
-        if item is None:
-            return {"mensagem":"ITEM NÃO ENCONSTRADO 404"}
+        cursorDB.execute('''SELECT * FROM cliente where cpf = %s''',(CPF_do_cliente,))
+        linhas_C = cursorDB.fetchall()
+
+        resultado_cliente = []
+        for linha in linhas_C:
+            resultado_cliente+=linha
+        if resultado_cliente== []:
+            return{"mensagem":"CLIENTE NÃO ENCONTRADO"}
+        
+        try:
+            cursorDB.execute('''SELECT * FROM produto where nome_produto = %s''',(nome_do_produto,))
+        except psycopg2.errors.UndefinedFunction:
+            return {"mensagem":"NOME DO PRODUTO INVALIDO"} 
         else:
-            db.delete(item)
-            db.commit()
-        return {"mensagem":f"id={item_id}, ITEM DELETADO"}
+
+            linhas_P = cursorDB.fetchall()
+
+            resultado_produto = []
+            for linha in linhas_P:
+                resultado_produto+=linha
+            if resultado_produto== []:
+                return{"mensagem":"PRODUTO NÃO ENCONTRADO"}
+            data_atual = datetime.now(ZoneInfo("America/Sao_Paulo"))
+            data_formatada = data_atual.strftime('%Y-%m-%d %H:%M:%S')
+            valorunit = resultado_produto[3]
+            valortotal = valorunit * quantidade
+
+            
+            
+            cursorDB.execute('''INSERT INTO vendas (id_cliente, id_produto, nome_produto, quantidade, valor,data_compra, data_confirmacao_compra) VALUES (%s, %s, %s, %s, %s, %s, %s)''',(resultado_cliente[0],resultado_produto[0], resultado_produto[1], quantidade, valortotal, str(data_formatada), None,))
+
+            cursorDB.execute('''SELECT id_venda FROM vendas WHERE id_cliente = %s ORDER BY data_compra DESC LIMIT 1''',(resultado_cliente[0],))
+            linhas_id_vd = cursorDB.fetchall()
+            resultado_id_vd = []
+            for linha in linhas_id_vd:
+                resultado_id_vd += linha
+
+            return {"mensagem":f"id venda({resultado_id_vd}), Produto comprado {nome_do_produto}*{quantidade} VALOR({valortotal})"}
+
+@app.put("/vendas/{id_venda}")
+def confirmar_venda(id_venda:int,cursorDB: object = Depends(criar_conexão)):
+    cursorDB.execute('''SELECT * FROM vendas WHERE id_venda = %s''',(id_venda,))
+    linhas_V = cursorDB.fetchall()
+
+    resultado_venda = []
+    for linha in linhas_V:
+        resultado_venda+=linha
+    if resultado_venda== []:
+        return{"mensagem":"VENDA NÃO ENCONTRADA"}
+    data_atual = datetime.now(ZoneInfo("America/Sao_Paulo"))
+    data_formatada = data_atual.strftime('%Y-%m-%d %H:%M:%S')
+
+    if resultado_venda[7] != None:
+        return {"mensagem":"VENDA JA HAVIA SIDO CONFIRMADA"}
+    
+    cursorDB.execute('''UPDATE vendas SET data_confirmacao_compra = %s WHERE id_venda = %s''',(data_formatada, id_venda,))
+
+    return{"mensagem":f"venda ({id_venda}) CONFIRMADA"}
